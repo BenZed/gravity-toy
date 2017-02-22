9be0060209503a4f639225bd9cf52e13497d71d8
BenGaumond <ben@globalmechanic.com>
2017-02-22 00:04:49 -0800

    ok
    haven’t tested it yet but i think i’m caught back up.

--- refactor-src/modules/simulation/simulation.js
+++ refactor-src/modules/simulation/simulation.js
@@ -3,7 +3,7 @@ import Integrator from './integrator'
 
 import is from 'is-explicit'
 
-import { floor, clamp } from 'math-plus'
+import { floor, cbrt, Vector } from 'math-plus'
 
 /******************************************************************************/
 // Constants
@@ -12,16 +12,16 @@ import { floor, clamp } from 'math-plus'
 const ONE_MB = 1048576 //bytes
 const NUMBER_SIZE = 8 // bytes
 const CACHABLE_PROPERTIES = 5
-const MIN_RADIUS = 0.25
+
+const MASS_MIN = 50
+const RADIUS_MIN = 0.5 //pixels
+const RADIUS_FACTOR = 0.5
+
 const ALLOCATIONS_PER_MB = ONE_MB / (NUMBER_SIZE * CACHABLE_PROPERTIES)
 
 const DEFAULT_PROPERTIES = {
 
   g: 1,
-  delta: 20, //milliseconds
-
-  radiusBase: 0.5, //pixels
-  radiusFactor: 0.25,
 
   maxCacheMemory: 320 // megabytes
 
@@ -33,32 +33,65 @@ const DEFAULT_PROPERTIES = {
 
 //constants for symbolic properties
 const CACHE = Symbol('cache'),
-  BODIES = Symbol('bodies'),
-  INTEGRATOR = Symbol('integrator')
+  INTEGRATOR = Symbol('integrator'),
+  INITIAL_TICK = Symbol('initial-tick'),
+  WRITE = Symbol('write')
 
-class Body {
-  constructor({pos, vel, mass}, id) {
+function Body ({mass, pos, vec}, tick, id) {
 
-    this.pos = pos
-    this.vel = vel
-    this.mass = mass
+  if (this == null)
+    //it actually doesn't, but I'm enforcing readable code. Sue me.
+    throw new Error('Body must be instanced.')
 
-    this.id = id
+  const body = function(tick, asArray) {
+
+    if (!isFinite(tick) || tick < 0)
+      return null
+
+    let i = (tick - this[INITIAL_TICK]) * CACHABLE_PROPERTIES
+    const cache = this[CACHE]
+
+    const mass = cache[i++]
+
+    //if we've gotten here, the requested tick has not been cached for this body
+    if (mass === undefined)
+      return null
+
+    const x = cache[i++]
+    const y = cache[i++]
+    const vx = cache[i++]
+    const vy = cache[i++]
+
+    return asArray
+      ? [mass, x, y, vx, vy]
+      : {
+        mass,
+        pos: new Vector(x,y),
+        vel: new Vector(x,y),
+        radius: radiusFromMass(mass)
+      }
 
   }
+
+  Define(body)
+    .const('id', id)
+    .const(CACHE, [])
+    .const(INITIAL_TICK, tick)
+    .const(WRITE, (mass, x, y, vx, vy) =>
+      this[CACHE].push(mass, x, y, vx, vy)
+    )
+
+  return body
+
 }
 
 function Cache(maxMemory) {
-
-  if (this == null)
-    throw new Error('Cache must be instanced.')
 
   Define(this)
 
     .let('id', 0)
     .let('tick', 0)
     .let('allocations', 0)
-    .const('maxMemory', maxMemory)
 
     .get('maxTicks', () => {
       const maxAllocations = this.maxMemory * ALLOCATIONS_PER_MB
@@ -67,22 +100,49 @@ function Cache(maxMemory) {
       return floor(this.tick / percentUsed)
     })
 
+    .const('maxMemory', maxMemory)
     .const('invalidate', (tick, before = false) => {})
-
     .const('read', tick => {
-      const bodies = []
-      for (const i in this) {
-        const storage = this[i]
-        bodies.push()
-      }// fuck, make this not suck
+
+      const output = []
+
+      //the only enumerable properties of a cache object will be bodies
+      for (const id in this) {
+        const body = this[id]
+        const data = body.read(tick, true)
+        if (!data)
+          continue
+
+        output.push(body.id, ...data)
+
+      }
+
+      return output
 
     })
 
-    .const('write', bodies => console.log('write cach', bodies))
+    .const('write', input => {
+
+      this.tick++
+
+      while (input.length) {
+        const [id, mass, x, y, vx, vy] = input.splice(0,6)
+        this[id].write(mass, x, y, vx, vy)
+      }
+
+      /* one liner for the kids:
+
+      this.tick++ && while(input.length) this[input.shift()].write(...input.splice(0,5))
+
+      */
+
+    })
 
 }
 
-export function calculateRadius(mass, base, factor) {
+function radiusFromMass(mass) {
+
+  return RADIUS_MIN + cbrt(mass - MASS_MIN) * RADIUS_FACTOR
 
 }
 
@@ -93,18 +153,15 @@ export default class Simulation {
     if (!is(props, Object))
       throw new TypeError('first argument, if defined, should be an Object.')
 
-    const { g, delta,
-      maxCacheMemory,
-      radiusBase,
-      radiusFactor } = { ...DEFAULT_PROPERTIES, ...props }
+    const { g, maxCacheMemory } = { ...DEFAULT_PROPERTIES, ...props }
 
     Define(this)
       .const.enum('g', g)
-      .const.enum('delta', delta)
       .const(CACHE, new Cache(maxCacheMemory))
       .const(INTEGRATOR, new Integrator(this[CACHE].write))
 
-    this[INTEGRATOR]('initialize', { g, delta })
+    this[INTEGRATOR]('initialize', { g })
+
   }
 
   start() {
@@ -125,18 +182,24 @@ export default class Simulation {
     if (!is(tick, Number))
       throw new Error('tick, if provided, is expected to be a number.')
 
+    if (is(props.pos) && !is(props.pos, Vector))
+      throw new Error('props.pos, if defined, is expected to be a Vector.')
+    props.pos = props.pos || Vector.zero
+
+    if (is(props.vel) && !is(props.vel, Vector))
+      throw new Error('props.vel, if defined, is expected to be a Vector.')
+    props.vel = props.vel || Vector.zero
+
+    if (!is(props.mass) || props.mass < MASS_MIN)
+      throw new Error(`props.mass, if defined, must be a number above or equal to ${MASS_MIN}`)
+    props.mass = props.mass || MASS_MIN
+
     if (tick < 0 || tick > cache.tick)
       throw new Error('tick out of range')
 
     const id = cache.id++
-    const body = new Body(props, id)
 
-    const storage = []
-    Define(storage)
-      .const('body', body)
-      .const('startTick', tick)
-
-    cache[id] = storage
+    cache[id] = new Body(props, tick, id)
     cache.invalidate(tick)
 
     this[INTEGRATOR]('set-bodies', cache.read(tick))
