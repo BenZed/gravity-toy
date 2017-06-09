fc99f77fc312105dbe7d4be7bf9771605f08a7c3
BenZed <bengaumond@gmail.com>
2017-06-08 18:33:27 -0700

    Fixed utils

--- src/modules/simulation/util/index.js
+++ src/modules/simulation/util/index.js
@@ -1,7 +1,8 @@
-import isOrderedFiniteArray from './radius-from-mass'
+import isOrderedFiniteArray from './is-ordered-finite-array'
+import radiusFromMass, { massFromRadius} from './radius-from-mass'
 import WeightedColorizer from './weighted-colorizer'
 import randomVector from './random-vector'
 import calcBaryCenter from './calculate-bary-center'
 
-export { isOrderedFiniteArray, radiusFromMass, massFromRadius, randomVector, calcBaryCenter,
-  WeightedColorizer }
+export { isOrderedFiniteArray, radiusFromMass, massFromRadius, randomVector,
+  calcBaryCenter, WeightedColorizer }
