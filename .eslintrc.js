9213964b2923b967cc2a240383c082d0025b88ad
BenZed <bengaumond@gmail.com>
2023-05-09 23:02:04 -0700

    format refactor
    all formatting done through eslint; eslint now uses prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,43 @@
 /* eslint-env node */
 
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier'
+    ],
     parser: '@typescript-eslint/parser',
     plugins: [
         '@typescript-eslint',
         'eslint-plugin-react',
-        'eslint-plugin-react-hooks'
+        'eslint-plugin-react-hooks',
+        'prettier'
     ],
-    root: true
+    root: true,
+    rules: {
+        'prettier/prettier': 'error',
+        '@typescript-eslint/restrict-plus-operands': 'off', // this rule seems to frequently break
+        '@typescript-eslint/no-unused-expressions': [
+            'error',
+            { allowTaggedTemplates: true }
+        ],
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            { args: 'all', argsIgnorePattern: '^_' }
+        ],
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/unified-signatures': 'off',
+        '@typescript-eslint/no-duplicate-imports': 'error',
+        '@typescript-eslint/no-this-alias': 'off', // I don't have a problem with 'this' aliasing.
+        '@typescript-eslint/explicit-member-accessibility': [
+            'error',
+            { accessibility: 'no-public' }
+        ],
+        '@typescript-eslint/explicit-function-return-type': [
+            'off',
+            { allowExpressions: true }
+        ]
+    }
 }
